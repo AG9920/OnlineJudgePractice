f9b15afeb458314660ef934b356016fb73518728
AG9920 <futuregong@126.com>
2016-06-03 11:56:04 +0800

    add hdu1001 1002

---
+++ src/com/futuregong/hdu1002.java
@@ -0,0 +1,54 @@
+package com.futuregong;
+
+import java.util.Scanner;
+
+/*
+ * A + B Problem II
+
+Time Limit: 2000/1000 MS (Java/Others)    Memory Limit: 65536/32768 K (Java/Others)
+Total Submission(s): 309935    Accepted Submission(s): 59933
+
+
+Problem Description
+I have a very simple problem for you. Given two integers A and B, your job is to calculate the Sum of A + B.
+ 
+
+Input
+The first line of the input contains an integer T(1<=T<=20) which means the number of test cases. 
+Then T lines follow, each line consists of two positive integers, A and B. 
+Notice that the integers are very large, that means you should not process them by using 32-bit integer. 
+You may assume the length of each integer will not exceed 1000.
+ 
+
+Output
+For each test case, you should output two lines. 
+The first line is "Case #:", # means the number of the test case. 
+The second line is the an equation "A + B = Sum", Sum means the result of A + B. 
+Note there are some spaces in the equation. Output a blank line between two test cases.
+ 
+
+Sample Input
+2
+1 2
+112233445566778899 998877665544332211
+ 
+
+Sample Output
+Case 1:
+1 + 2 = 3
+
+Case 2:
+112233445566778899 + 998877665544332211 = 1111111111111111110
+ */
+
+public class hdu1002 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();//n represents for the amount of test cases
+		while(n-->0){
+			
+		}
+	}
+	
+
+}
